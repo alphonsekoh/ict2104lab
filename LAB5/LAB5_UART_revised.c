c997c8054055003e94c01f5070ebee02ee006ff9
alphonsekoh <alphonsekoh@gmail.com>
2021-10-20 16:04:47 +0800

    Add: UART original code and revised code

---
+++ LAB5/LAB5_UART_revised.c
@@ -0,0 +1,144 @@
+/* --COPYRIGHT--,BSD
+ * Copyright (c) 2017, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * --/COPYRIGHT--*/
+/******************************************************************************
+ * Debugging log via MSP432 UART
+ *
+ * Description: This demo prints debugging log via a PC serial port (i.e. COM).
+ * SMCLK/DCO is used as clock source and auto-clock enable feature is used by eUSCI.
+ * COM used is described as "XDS110 Class Application/User UART".
+ * Check your device manager to find out which COM port is used, e.g. COM3
+ *
+ *               MSP432P401
+ *             -----------------
+ *            |                 |
+ *            |                 |
+ *       RST -|     P1.3/UCA0TXD|----> PC
+ *            |                 |
+ *            |     P1.2/UCA0RXD|<---- PC
+ *            |                 |
+ *
+ *******************************************************************************/
+/* DriverLib Includes */
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
+/* Standard Includes */
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+void uPrintf(unsigned char * TxArray);
+
+//![Simple UART Config]
+/* UART Configuration Parameter. These are the configuration parameters to
+ * make the eUSCI A UART module to operate with a 9600 baud rate. These
+ * values were calculated using the online calculator that TI provides at:
+ * http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSP430BaudRateConverter/index.html
+ */
+const eUSCI_UART_ConfigV1 uartConfig =
+{
+        EUSCI_A_UART_CLOCKSOURCE_SMCLK,                 // SMCLK Clock Source
+        78,                                             // BRDIV = 78
+        2,                                              // UCxBRF = 2
+        0,                                              // UCxBRS = 0
+        EUSCI_A_UART_ODD_PARITY,                        // ODD Parity
+        EUSCI_A_UART_LSB_FIRST,                         // LSB First
+        EUSCI_A_UART_ONE_STOP_BIT,                      // One stop bit
+        EUSCI_A_UART_MODE,                              // UART mode
+        EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION,  // Oversampling
+        EUSCI_A_UART_8_BIT_LEN                          // 8 bit data length
+};
+//![Simple UART Config]
+
+int main(void)
+{
+    /* Halting WDT  */
+    WDT_A_holdTimer();
+
+    /* Selecting P1.2 and P1.3 in UART mode */
+    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1, GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+
+    /* Setting DCO to 12MHz */
+    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_12);
+
+    /* Configuring UART Module */
+    UART_initModule(EUSCI_A0_BASE, &uartConfig);
+
+    /* Enable UART module */
+    UART_enableModule(EUSCI_A0_BASE);
+
+    /* Enabling interrupts (Rx) */
+    UART_enableInterrupt(EUSCI_A0_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
+    Interrupt_enableInterrupt(INT_EUSCIA0);
+    Interrupt_enableMaster();
+
+    uPrintf("Going to Sleep\n\r");
+
+    while(1)
+    {
+        PCM_gotoLPM3InterruptSafe();
+    }
+}
+
+void uPrintf(unsigned char * TxArray)
+{
+    unsigned short i = 0;
+    while(*(TxArray+i))
+    {
+        UART_transmitData(EUSCI_A0_BASE, *(TxArray+i));  // Write the character at the location specified by pointer
+        i++;                                             // Increment pointer to point to the next character
+    }
+}
+
+/* EUSCI A0 UART ISR */
+void EUSCIA0_IRQHandler(void)
+{
+    
+    unsigned char a = 0;
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+    a = UART_receiveData(EUSCI_A0_BASE);
+    /*Trigger LED 1 from UART Serial TX*/
+    if(a == 'a'){
+        // if input from UART is an 'a' LED 1 will light up
+        MAP_GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
+    }
+    if(a == 'b'){
+        // if input from UART is an 'a' LED 1 will power down
+        MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+    }
+    
+    // if char is lowercase, convert it to uppercase 
+    // substract 32 from ascii
+    if (a >= 'a' && a <= 'z'){
+            a = a - 32;
+    }
+    UART_transmitData(EUSCI_A0_BASE, a);
+}
